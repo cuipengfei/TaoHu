116816d80146ec9943faa7e51a56d265b3859766
cuipengfei <cuipengfei2008@gmail.com>
2013-04-16 18:53:04 +0800

    remove scala

--- src/main/java/taohu/JavaHelloWorld.java
+++ src/main/java/taohu/JavaHelloWorld.java
@@ -1,12 +1,7 @@
 package taohu;
 
 public class JavaHelloWorld {
-    public void test() {
-        ScalaHelloWorld scalaHelloWorld = new ScalaHelloWorld();
-        scalaHelloWorld.scalaSayHello();
-    }
-
-    public void javaSayHello() {
-        System.out.println("Java Says Hello, World!");
+    public String javaSayHello() {
+        return "Java Says Hello, World!";
     }
 }
--- src/test/java/taohu/JavaHelloWorldTest.java
+++ src/test/java/taohu/JavaHelloWorldTest.java
@@ -1,11 +1,16 @@
 package taohu;
 
+import org.junit.Assert;
 import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
 
 public class JavaHelloWorldTest {
     @Test
     public void testTest() throws Exception {
         JavaHelloWorld javaHelloWorld = new JavaHelloWorld();
-        javaHelloWorld.test();
+        String text = javaHelloWorld.javaSayHello();
+
+        Assert.assertThat(text, is("Java Says Hello, World!"));
     }
 }
