57ab9743e2b7bbc05539421d8b4b5bc09f2f839d
Kyle306 <kyle306@gmail.com>
2013-04-16 20:18:34 +0800

    setup tck tests and add ignore file

---
+++ src/test/java/taohu/TCKTest.java
@@ -0,0 +1,21 @@
+package taohu;
+
+import org.atinject.tck.Tck;
+import org.atinject.tck.auto.Convertible;
+import org.junit.Test;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: twer
+ * Date: 13-4-16
+ * Time: 下午8:03
+ * To change this template use File | Settings | File Templates.
+ */
+public class TCKTest {
+
+    @Test
+    public void testCar(){
+        Tck.testsFor(null, false, false);
+    }
+
+}
