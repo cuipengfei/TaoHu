437a01ad95b12983453e9db1be9207a9ea4f5d11
Kyle306 <kyle306@gmail.com>
2013-04-18 21:52:25 +0800

    change signature of parser

--- src/main/java/taohu/parser/XmlBeanParser.java
+++ src/main/java/taohu/parser/XmlBeanParser.java
@@ -9,26 +9,27 @@ import javax.xml.validation.SchemaFactory;
 import javax.xml.validation.Validator;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.List;
 
 public class XmlBeanParser {
 
-    private String fileName;
 
-    public XmlBeanParser(String fileName) {
-        this.fileName = fileName;
+    private InputStream xmlFileInputStream;
+
+    public XmlBeanParser(InputStream xmlFileInputStream) {
+        this.xmlFileInputStream = xmlFileInputStream;
     }
 
     public List<BeanDescriptor> parseBeans() throws SAXException, IOException, URISyntaxException {
         ClassLoader classLoader = getClass().getClassLoader();
         URL xsdUrl = classLoader.getResource("taohu/bean.xsd");
-        URL xmlUrl = classLoader.getResource(fileName);
 
         SchemaFactory schemaFactory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
         Validator validator = schemaFactory.newSchema(xsdUrl).newValidator();
-        validator.validate(new StreamSource(new File(xmlUrl.toURI())));
+        validator.validate(new StreamSource(xmlFileInputStream));
 
         return null;
     }
--- src/test/java/taohu/parser/XmlBeanParserTest.java
+++ src/test/java/taohu/parser/XmlBeanParserTest.java
@@ -3,11 +3,14 @@ package taohu.parser;
 import org.junit.Test;
 import org.xml.sax.SAXException;
 
+import java.io.InputStream;
+
 public class XmlBeanParserTest {
 
     @Test(expected = SAXException.class)
     public void shouldThrowExceptionWhenXMLFileCouldNotPassXSDValidation() throws Exception {
-        XmlBeanParser parser = new XmlBeanParser("taohu/illegalXmlFile.xml");
+        InputStream xmlFileInputStream = getClass().getClassLoader().getResourceAsStream("taohu/illegalXmlFile.xml");
+        XmlBeanParser parser = new XmlBeanParser(xmlFileInputStream);
         parser.parseBeans();
     }
 }
