86aa8f340754cbcfc6f554cac653ae483f4baea0
cuipengfei <cuipengfei2008@gmail.com>
2013-04-18 22:20:02 +0800

    rename test

--- src/test/java/taohu/inject/DependencyInjectorSetterTest.java
+++ src/test/java/taohu/inject/DependencyInjectorSetterTest.java
@@ -32,7 +32,7 @@ public class DependencyInjectorSetterTest {
     }
 
     @Test
-    public void shouldNotCallSetterWithInjectAndProvidePara()
+    public void shouldNotCallSetterWithoutInject()
             throws Exception {
         DependencyInjector dependencyInjector = new DependencyInjector();
         Object instance = dependencyInjector.createInstanceAndInjectDependencies("taohu.inject.setter.SetterWithoutInject");
