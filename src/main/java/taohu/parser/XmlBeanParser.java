0cf2e06fe82adf5c82732ebe4bf8a3319601a1ac
Kyle306 <kyle306@gmail.com>
2013-04-19 00:16:59 +0800

    add tests for create BeanDescriptor itself

--- src/main/java/taohu/parser/XmlBeanParser.java
+++ src/main/java/taohu/parser/XmlBeanParser.java
@@ -1,37 +1,74 @@
 package taohu.parser;
 
+import org.w3c.dom.Document;
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 import taohu.model.BeanDescriptor;
 
 import javax.xml.XMLConstants;
+import javax.xml.bind.Element;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParserFactory;
 import javax.xml.transform.stream.StreamSource;
 import javax.xml.validation.SchemaFactory;
 import javax.xml.validation.Validator;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.List;
 
 public class XmlBeanParser {
 
+    private URI xmlURI;
 
-    private InputStream xmlFileInputStream;
-
-    public XmlBeanParser(InputStream xmlFileInputStream) {
-        this.xmlFileInputStream = xmlFileInputStream;
+    public XmlBeanParser(URI xmlURI) {
+        this.xmlURI = xmlURI;
     }
 
-    public List<BeanDescriptor> parseBeans() throws SAXException, IOException, URISyntaxException {
+    public List<BeanDescriptor> parseBeans() throws SAXException, IOException, URISyntaxException, ParserConfigurationException, ClassNotFoundException {
+
+        validateXML();
+
+        return parseBeanDescriptors();
+    }
+
+    private List<BeanDescriptor> parseBeanDescriptors() throws ParserConfigurationException, IOException, SAXException, ClassNotFoundException {
+        DocumentBuilder builder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+        Document document = builder.parse(xmlURI.toString());
+        NodeList nodeList = document.getElementsByTagName("bean");
+
+        List<BeanDescriptor> beans = new ArrayList();
+
+        for (int idx = 0; idx < nodeList.getLength(); idx++) {
+            BeanDescriptor descriptor = new BeanDescriptor();
+            Node node = nodeList.item(idx);
+
+            NamedNodeMap attributeMap = node.getAttributes();
+            descriptor.setId(attributeMap.getNamedItem("id").getNodeValue());
+            descriptor.setClazz(Class.forName(attributeMap.getNamedItem("class").getNodeValue()));
+
+            beans.add(descriptor);
+        }
+
+        return beans;
+    }
+
+    private void validateXML() throws SAXException, IOException {
         ClassLoader classLoader = getClass().getClassLoader();
         URL xsdUrl = classLoader.getResource("taohu/bean.xsd");
 
         SchemaFactory schemaFactory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
         Validator validator = schemaFactory.newSchema(xsdUrl).newValidator();
-        validator.validate(new StreamSource(xmlFileInputStream));
-
-        return null;
+        validator.validate(new StreamSource(new File(xmlURI)));
     }
 }
 
--- src/test/java/taohu/parser/XmlBeanParserTest.java
+++ src/test/java/taohu/parser/XmlBeanParserTest.java
@@ -2,15 +2,35 @@ package taohu.parser;
 
 import org.junit.Test;
 import org.xml.sax.SAXException;
+import taohu.model.BeanDescriptor;
 
 import java.io.InputStream;
+import java.net.URI;
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class XmlBeanParserTest {
 
     @Test(expected = SAXException.class)
     public void shouldThrowExceptionWhenXMLFileCouldNotPassXSDValidation() throws Exception {
-        InputStream xmlFileInputStream = getClass().getClassLoader().getResourceAsStream("taohu/illegalXmlFile.xml");
-        XmlBeanParser parser = new XmlBeanParser(xmlFileInputStream);
+        URI xmlURI = getClass().getClassLoader().getResource("taohu/illegalXmlFile.xml").toURI();
+        XmlBeanParser parser = new XmlBeanParser(xmlURI);
         parser.parseBeans();
+    }
+
+    @Test
+    public void shouldCreateBeanDescriptorAccordingToXML() throws Exception {
+        URI xmlURI = getClass().getClassLoader().getResource("taohu/legalXmlFile.xml").toURI();
+        XmlBeanParser parser = new XmlBeanParser(xmlURI);
+        List<BeanDescriptor> beans = parser.parseBeans();
+
+        assertThat(beans, notNullValue());
+        assertThat(beans.size(), is(2));
+        assertThat(beans.get(0).getId(), is("first"));
+        assertThat(beans.get(0).getClazz().equals(String.class), is(true));
+        assertThat(beans.get(1).getId(), is("second"));
+        assertThat(beans.get(1).getClazz().equals(Integer.class), is(true));
     }
 }
