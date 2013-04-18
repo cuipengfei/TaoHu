a67d9fdbe5c9f3e71663f3fb4bbf566dc78c9cd4
cuipengfei <cuipengfei2008@gmail.com>
2013-04-18 23:29:11 +0800

    prep TCK

--- src/test/java/taohu/TCKTest.java
+++ src/test/java/taohu/TCKTest.java
@@ -1,14 +1,18 @@
 package taohu;
 
 import org.atinject.tck.Tck;
+import org.atinject.tck.auto.Car;
 import org.junit.Ignore;
 import org.junit.Test;
+import taohu.inject.DependencyInjector;
 
 @Ignore
 public class TCKTest {
 
     @Test
-    public void testCar() {
-        Tck.testsFor(null, false, false);
+    public void testCar() throws Exception {
+        Tck.testsFor((Car) new DependencyInjector().
+                createInstanceAndInjectDependencies("org.atinject.tck.auto.Convertible"),
+                false, false);
     }
 }
