8c6402cd151fee59316d4d1d6731ed0b1b671382
Kyle306 <kyle306@gmail.com>
2013-04-22 23:13:46 +0800

    refactor

--- src/test/java/taohu/Resolver/BeanObjectFactoryTest.java
+++ src/test/java/taohu/Resolver/BeanObjectFactoryTest.java
@@ -1,5 +1,6 @@
 package taohu.Resolver;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
 import taohu.inject.ctor.AnnotatedNoParaCtor;
 import taohu.inject.ctor.OneParaCtor;
@@ -15,47 +16,33 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 public class BeanObjectFactoryTest {
 
+    private static BeanObjectFactory beanObjectFactory;
+
     @Test
     public void shouldGetBeanObjectWithoutContructorArgumentsAndProperties() throws Exception {
-        BeanDescriptor beanDescriptor = new BeanDescriptor();
-        beanDescriptor.setId("String");
-        beanDescriptor.setClazz(String.class);
-
-        List<BeanDescriptor> beanDescriptors = new ArrayList<>();
-        beanDescriptors.add(beanDescriptor);
-
-        BeanObjectFactory factory = new BeanObjectFactory(beanDescriptors);
-        Object object = factory.getBean(beanDescriptor.getId());
-        assertThat(object.getClass().equals(String.class), is(true));
+        Object object = beanObjectFactory.getBean("noParaCtor");
+        assertThat(object.getClass().equals(AnnotatedNoParaCtor.class), is(true));
     }
 
     @Test
     public void shouldGetBeanObejctWithOneConstructorArguments() throws Exception {
-        BeanDescriptor noParaDescriptor = new BeanDescriptor();
-        noParaDescriptor.setId("noParaCtor");
-        noParaDescriptor.setClazz(AnnotatedNoParaCtor.class);
-
-        ConstructorArgumentDescriptor argumentDescriptor = new ConstructorArgumentDescriptor();
-        argumentDescriptor.setBeanId("noParaCtor");
-
-        BeanDescriptor oneParaDescriptor = new BeanDescriptor();
-        oneParaDescriptor.setId("oneParaCtor");
-        oneParaDescriptor.setClazz(OneParaCtor.class);
-        oneParaDescriptor.getConstructorDependency().add(argumentDescriptor);
-
-
-        List<BeanDescriptor> beanDescriptors = new ArrayList<>();
-        beanDescriptors.add(noParaDescriptor);
-        beanDescriptors.add(oneParaDescriptor);
-
-        BeanObjectFactory factory = new BeanObjectFactory(beanDescriptors);
-        Object object = factory.getBean("oneParaCtor");
+        Object object = beanObjectFactory.getBean("oneParaCtor");
         assertThat(object.getClass().equals(OneParaCtor.class), is(true));
-        assertThat(((OneParaCtor)object).getAnnotatedNoParaCtor().getClass().equals(AnnotatedNoParaCtor.class), is(true));
+        assertThat(((OneParaCtor) object).getAnnotatedNoParaCtor().getClass().equals(AnnotatedNoParaCtor.class), is(true));
     }
 
     @Test
     public void shouldGetBeanObejctWithTwoConstructorArguments() throws Exception {
+        Object object = beanObjectFactory.getBean("twoParaCtor");
+        assertThat(object.getClass().equals(TwoParaCtor.class), is(true));
+        assertThat(((TwoParaCtor) object).getAnnotatedNoParaCtor().getClass().equals(AnnotatedNoParaCtor.class), is(true));
+        assertThat(((TwoParaCtor) object).getOneParaCtor().getClass().equals(OneParaCtor.class), is(true));
+        assertThat(((TwoParaCtor) object).getOneParaCtor().getAnnotatedNoParaCtor().getClass()
+                .equals(AnnotatedNoParaCtor.class), is(true));
+    }
+
+    @BeforeClass
+    public static void setUp() {
         BeanDescriptor noParaDescriptor = new BeanDescriptor();
         noParaDescriptor.setId("noParaCtor");
         noParaDescriptor.setClazz(AnnotatedNoParaCtor.class);
@@ -83,13 +70,6 @@ public class BeanObjectFactoryTest {
         beanDescriptors.add(oneParaDescriptor);
         beanDescriptors.add(twoParaDescriptor);
 
-
-        BeanObjectFactory factory = new BeanObjectFactory(beanDescriptors);
-        Object object = factory.getBean("twoParaCtor");
-        assertThat(object.getClass().equals(TwoParaCtor.class), is(true));
-        assertThat(((TwoParaCtor)object).getAnnotatedNoParaCtor().getClass().equals(AnnotatedNoParaCtor.class), is(true));
-        assertThat(((TwoParaCtor)object).getOneParaCtor().getClass().equals(OneParaCtor.class), is(true));
-        assertThat(((TwoParaCtor)object).getOneParaCtor().getAnnotatedNoParaCtor().getClass()
-                .equals(AnnotatedNoParaCtor.class), is(true));
+        beanObjectFactory = new BeanObjectFactory(beanDescriptors);
     }
 }
