9a845d939554d7ede249a75ebce8f5ab50c4a297
Kyle306 <kyle306@gmail.com>
2013-04-22 22:34:55 +0800

    create Object instance with one argument

--- src/main/java/taohu/Resolver/BeanObjectFactory.java
+++ src/main/java/taohu/Resolver/BeanObjectFactory.java
@@ -2,9 +2,11 @@ package taohu.Resolver;
 
 import taohu.inject.exception.BeanInitializationException;
 import taohu.model.BeanDescriptor;
+import taohu.model.ConstructorArgumentDescriptor;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -27,8 +29,25 @@ public class BeanObjectFactory {
         Object object = null;
         try {
             Constructor constructor = null;
-            constructor = beanDescriptor.getClazz().getConstructor();
-            object = constructor.newInstance();
+
+            if (beanDescriptor.getConstructorDependency() != null && beanDescriptor.getConstructorDependency().size() > 0) {
+                List<Class> argumentTypes = new ArrayList<>();
+                List<Object> argumentObjects = new ArrayList<>();
+                for (ConstructorArgumentDescriptor argumentDescriptor : beanDescriptor.getConstructorDependency()) {
+                    String argumentBeanId = argumentDescriptor.getBeanId();
+                    BeanDescriptor argumentBeanDescriptor = beanDescriptors.get(argumentBeanId);
+                    argumentTypes.add(argumentBeanDescriptor.getClazz());
+                    Object argumentObject = this.getBean(argumentBeanId);
+                    argumentObjects.add(argumentObject);
+                }
+                Class[] argTypes = new Class[argumentTypes.size()];
+                constructor = beanDescriptor.getClazz().getConstructor(argumentTypes.toArray(argTypes));
+                object = constructor.newInstance(argumentObjects.toArray());
+            } else {
+                constructor = beanDescriptor.getClazz().getConstructor();
+                object = constructor.newInstance();
+            }
+
         } catch (NoSuchMethodException e) {
             throw new BeanInitializationException("Could not find such method to create bean", e);
         } catch (InstantiationException e) {
--- src/test/java/taohu/Resolver/BeanObjectFactoryTest.java
+++ src/test/java/taohu/Resolver/BeanObjectFactoryTest.java
@@ -1,7 +1,10 @@
 package taohu.Resolver;
 
 import org.junit.Test;
+import taohu.inject.ctor.AnnotatedNoParaCtor;
+import taohu.inject.ctor.OneParaCtor;
 import taohu.model.BeanDescriptor;
+import taohu.model.ConstructorArgumentDescriptor;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -23,5 +26,30 @@ public class BeanObjectFactoryTest {
         BeanObjectFactory factory = new BeanObjectFactory(beanDescriptors);
         Object object = factory.getBean(beanDescriptor.getId());
         assertThat(object.getClass().equals(String.class), is(true));
+    }
+
+    @Test
+    public void shouldGetbeanObejctWithConstructorArguments() throws Exception {
+        BeanDescriptor noParaDescriptor = new BeanDescriptor();
+        noParaDescriptor.setId("noParaCtor");
+        noParaDescriptor.setClazz(AnnotatedNoParaCtor.class);
+
+        ConstructorArgumentDescriptor argumentDescriptor = new ConstructorArgumentDescriptor();
+        argumentDescriptor.setBeanId("noParaCtor");
+
+        BeanDescriptor oneParaDescriptor = new BeanDescriptor();
+        oneParaDescriptor.setId("oneParaCtor");
+        oneParaDescriptor.setClazz(OneParaCtor.class);
+        oneParaDescriptor.getConstructorDependency().add(argumentDescriptor);
+
+
+        List<BeanDescriptor> beanDescriptors = new ArrayList<>();
+        beanDescriptors.add(noParaDescriptor);
+        beanDescriptors.add(oneParaDescriptor);
+
+        BeanObjectFactory factory = new BeanObjectFactory(beanDescriptors);
+        Object object = factory.getBean("oneParaCtor");
+        assertThat(object.getClass().equals(OneParaCtor.class), is(true));
+        assertThat(((OneParaCtor)object).getAnnotatedNoParaCtor().getClass().equals(AnnotatedNoParaCtor.class), is(true));
     }
 }
