dfb9bf60722784dc7de142c10518fb66d6ce2c21
Kyle306 <kyle306@gmail.com>
2013-04-23 21:34:46 +0800

    add bean configuration resolver

---
+++ src/main/java/taohu/inject/impl/BeanConfigurationResolverImp.java
@@ -0,0 +1,30 @@
+package taohu.inject.impl;
+
+import com.google.common.base.Predicate;
+import com.google.common.collect.Iterables;
+import taohu.inject.interfaces.BeanConfigurationResolver;
+import taohu.model.BeanDescriptor;
+
+import javax.annotation.Nullable;
+import java.util.List;
+
+public class BeanConfigurationResolverImp implements BeanConfigurationResolver {
+
+    private List<BeanDescriptor> beanDescriptors;
+
+    public BeanConfigurationResolverImp(List<BeanDescriptor> beanDescriptors) {
+
+        this.beanDescriptors = beanDescriptors;
+    }
+
+    @Override
+    public boolean containsBean(final Class<?> clazz) {
+        boolean any = Iterables.any(beanDescriptors, new Predicate<BeanDescriptor>() {
+            @Override
+            public boolean apply(@Nullable BeanDescriptor input) {
+                return input.getClazz().equals(clazz);
+            }
+        });
+        return any;
+    }
+}
---
+++ src/test/java/taohu/inject/impl/BeanConfigurationResolverImpTest.java
@@ -0,0 +1,37 @@
+package taohu.inject.impl;
+
+import org.junit.Before;
+import org.junit.Test;
+import taohu.inject.ctor.AnnotatedNoParaCtor;
+import taohu.inject.ctor.OneParaCtor;
+import taohu.inject.interfaces.BeanConfigurationResolver;
+import taohu.model.BeanDescriptor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class BeanConfigurationResolverImpTest {
+
+    private List<BeanDescriptor> beanDescriptors;
+
+    @Before
+    public void setUp() throws Exception {
+        BeanDescriptor onePara = new BeanDescriptor("onePara", OneParaCtor.class);
+
+        this.beanDescriptors = new ArrayList<>();
+        beanDescriptors.add(onePara);
+    }
+
+    @Test
+    public void shouldKnowContainsBeanOrNot() throws Exception {
+        BeanConfigurationResolver resolver = new BeanConfigurationResolverImp(this.beanDescriptors);
+        boolean shouldContain = resolver.containsBean(OneParaCtor.class);
+        boolean shouldNotContain = resolver.containsBean(AnnotatedNoParaCtor.class);
+
+        assertThat(shouldContain, is(true));
+        assertThat(shouldNotContain, is(false));
+    }
+}
